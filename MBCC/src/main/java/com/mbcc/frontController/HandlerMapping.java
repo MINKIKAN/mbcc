4d88434144328f8528250c439045bb7c23d76845
jioneLEE <ehdwn1489@naver.com>
2023-03-15 09:36:12 +0900

    .
    .

---
+++ MBCC/src/main/java/com/mbcc/frontController/HandlerMapping.java
@@ -0,0 +1,14 @@
+package com.mbcc.frontController;
+
+import java.util.HashMap;
+
+public class HandlerMapping {
+  private HashMap<String, Controller> mappings;
+  public HandlerMapping() { 
+	  mappings=new HashMap<String, Controller>();
+
+  }
+  public Controller getController(String key) { 
+	  return mappings.get(key);
+  }
+}
---
+++ MBCC/src/main/java/com/mbcc/frontController/MbccFrontController.java
@@ -0,0 +1,39 @@
+package com.mbcc.frontController;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class MbccFrontController extends HttpServlet {
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		req.setCharacterEncoding("utf-8");
+		String url=req.getRequestURI();
+		System.out.println("url=" + url); 
+		String ctx=req.getContextPath();
+		String command=url.substring(ctx.length());
+		System.out.println("command=" + command); 
+		Controller controller=null;
+		String nextPage=null;
+	    HandlerMapping mapping=new HandlerMapping();
+	    controller=mapping.getController(command);
+	    
+	    nextPage=controller.requestHandler(req, res);
+
+	
+		if(nextPage!=null) {
+			if(nextPage.indexOf("redirect:")!=-1) {
+				res.sendRedirect( ctx+ nextPage.split(":")[1]); 
+			}else {
+				RequestDispatcher rd=req.getRequestDispatcher(ViewResolver.makeView(nextPage)); 
+				rd.forward(req, res);
+			}
+		}		
+	}
+}
