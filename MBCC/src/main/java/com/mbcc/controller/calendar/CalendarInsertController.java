dac96598d322cb40235015ff93db2510d4a7860a
MINKIKAN <alsrl568@gmail.com>
2023-03-24 15:09:09 +0900

    add2
    add2

--- MBCC/src/main/java/com/mbcc/controller/calendar/CalendarInsertController.java
+++ MBCC/src/main/java/com/mbcc/controller/calendar/CalendarInsertController.java
@@ -11,17 +11,15 @@ import com.mbcc.frontController.Controller;
 import com.mbcc.vo.CalVo;
 
 public class CalendarInsertController implements Controller {
-
 	@Override
 	public String requestHandler(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		 String title = req.getParameter("title");
-	        String start = req.getParameter("start");
-	        String end = req.getParameter("end");
-	        
-	        CalVo c = new CalVo(1, title, start, end);
-	        calendarDAO.getInstance().saveSchedule(c);
-	        return null;
+		String title = req.getParameter("title");
+		String start = req.getParameter("start");
+		String end = req.getParameter("end");
 
+		CalVo c = new CalVo(1, title, start, end);
+		calendarDAO.getInstance().saveSchedule(c);
+		return null;
 	}
 
 }
